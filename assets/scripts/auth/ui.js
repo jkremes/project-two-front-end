46c4e496b0b246a111ed2ae0db5f6908490fb7b0
jkremes <only1truth333@gmail.com>
2018-10-18 15:13:20 -0400

    I created the initial mold for my browser.
    I essentially used the same code from my tic-tac-toe app that I know works. Everything works for the localhost.
    
    Next step is to change the config file to include my herokuapp url.

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,67 @@
+const store = require('../store.js')
+
+const signUpSuccess = function () {
+  $('#login').html('Sign up success!')
+  $('#login').css('color', 'green')
+  $('#sign-up-form').trigger('reset')
+}
+
+const signUpFailure = function () {
+  $('#login').html('Somthing went wrong, please try again')
+  $('#login').css('color', 'red')
+  $('#sign-up-form').trigger('reset')
+}
+
+const signInSuccess = function (response) {
+  $('#login').html('Sign in success!')
+  $('#login').css('color', 'green')
+  $('#sign-in-form').trigger('reset')
+  store.user = response.user
+  $('#sign-up-form').addClass('hidden')
+  $('#sign-in-form').addClass('hidden')
+  $('#sign-out-button').removeClass('hidden')
+  $('#create-new-game').removeClass('hidden')
+  $('#change-password-form').removeClass('hidden')
+  $('#check-games-played').removeClass('hidden')
+}
+
+const signInFailure = function () {
+  $('#login').html('Something went wrong, please try again')
+  $('#login').css('color', 'red')
+  $('#sign-in-form').trigger('reset')
+}
+
+const changePasswordSuccess = function () {
+  $('#login').html('Change password success!')
+  $('#login').css('color', 'green')
+  $('#sign-up-form').trigger('reset')
+  $('#change-password-form').removeClass('hidden')
+}
+
+const changePasswordFailure = function () {
+  $('#login').html('Somthing went wrong, please try again')
+  $('#login').css('color', 'red')
+  $('#sign-up-form').trigger('reset')
+}
+
+const signOutSuccess = function () {
+  $('#login').html('You are signed out!')
+  $('#login').css('color', 'green')
+  $('#sign-up-form').removeClass('hidden')
+  $('#sign-in-form').removeClass('hidden')
+}
+
+const signOutFailure = function () {
+  $('#login').html('Something went wrong, please try again')
+  $('#login').css('color', 'red')
+}
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
